0f0b3e61832497e36432729dbe26fccfc56df3b8
Kunlesymls <Kunlesymls@DESKTOP-NIN1GDM>
2017-04-16 09:42:45 +0100

    Add project files.

---
+++ FollowUpWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FollowUpWebApp.Startup))]
+namespace FollowUpWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
